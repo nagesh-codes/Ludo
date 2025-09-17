51aed9fcc020a25190ad3265389a686dab239c58
nagesh-codes <nageshcodes0143@gmail.com>
2025-09-17 19:57:53 +0530

    complted the ui for waiting area page

--- client/src/pages/Waiting_Area.jsx
+++ client/src/pages/Waiting_Area.jsx
@@ -4,15 +4,16 @@ import QRCode from 'qrcode';
 import '../css-files/Waiting_Area.css';
 
 const Waiting_Area = () => {
-  const maxplayer = sessionStorage.getItem('maxPlayer') || 3
-  const [pla_name, setPla_name] = useState(['Nagesh Gh']);
+  const maxplayer = sessionStorage.getItem('maxPlayer') || 4
+  const [pla_name, setPla_name] = useState(['Nagesh Gh', 'mahesh c']);
 
   useEffect(() => {
     for (let i = 0; i < maxplayer - 1; i++) {
       pla_name.push(NaN);
     }
+    // pla_name.
     QRCode.toCanvas(document.getElementById("canvas"), window.location.origin, {
-      width: window.innerWidth < 600 ? 180 : 300,
+      width: window.innerWidth < 600 ? 180 : 330,
       color: {
         dark: "#ffffff",
         light: "#1ec4d9"
@@ -31,17 +32,23 @@ const Waiting_Area = () => {
         <div className="heading">Lobby Name</div>
         <div className="middle">
           <div className="left">
-            <div className="qrcode">
-              <canvas id='canvas'></canvas>
+            <div className="player-area">
+              <p>Players</p>
+              <div className="player-status">
+                {renderPlayers(maxplayer, pla_name)}
+              </div>
+            </div>
+            <div className="info">
+              Hold tight! We’re setting up your game room. This screen will stay up until all players have joined. Once everyone is connected, the game will begin automatically. Get ready for some fun!
             </div>
           </div>
           <div className="right">
-            <div className="player-status">
-              {renderPlayers(maxplayer, pla_name)}
+            <div className="qrcode">
+              <canvas id='canvas'></canvas>
             </div>
-            <div className="info">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab dicta iure dignissimos nihil vitae perferendis autem, ipsa soluta quisquam porro amet facilis similique blanditiis velit corporis quidem dolores. Magnam, earum!
-            </div>
+            <button className="shr-btn">
+              Invite Friends
+            </button>
           </div>
         </div>
       </div>
@@ -58,7 +65,7 @@ function renderPlayers(maxplayer, pla_name) {
           <Avatar
             round={true}
             name={pla_name[i]}
-            size={50}
+            size={window.innerWidth < 600 ? 40 : 50}
             textSizeRatio={2.5}
             className="avt"
             color="#7c5eff"
